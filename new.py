5fdbcca0a657cbdaf862a7cd91ee2bd6441138ff
Shasmitha <shasmitharam@gmail.com>
2025-04-12 10:52:19 +0530

    Added content to app.py, chatbot.py, intents.json, and requirements.txt

---
+++ new.py
@@ -0,0 +1,79 @@
+import random
+import json
+import pickle
+import numpy as np
+import tensorflow as tf
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+# Load intents file
+intents = json.loads(open('C:/Users/shasm/Downloads/chatbot/intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignoreLetters = ['?', '!', '.', ',']
+
+# Process the intents and create the words and classes
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        wordList = nltk.word_tokenize(pattern)
+        words.extend(wordList)
+        documents.append((wordList, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Lemmatize and sort the words
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLetters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+# Save the words and classes to pickle files
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# Prepare the training data
+training = []
+outputEmpty = [0] * len(classes)
+
+# Create the bag of words and output row for each document
+for document in documents:
+    bag = []
+    wordPatterns = document[0]
+    wordPatterns = [lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
+    for word in words:
+        bag.append(1) if word in wordPatterns else bag.append(0)
+
+    outputRow = list(outputEmpty)
+    outputRow[classes.index(document[1])] = 1
+    training.append(bag + outputRow)
+
+# Shuffle the training data
+random.shuffle(training)
+training = np.array(training)
+
+trainX = training[:, :len(words)]
+trainY = training[:, len(words):]
+
+# Build the model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
+
+# Compile the model with Stochastic Gradient Descent (SGD)
+sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train the model
+hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
+
+# Save the trained model
+model.save('chatbot_model.h5')
+
+print('Training complete and model saved!')
