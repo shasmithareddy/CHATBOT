40e7a62c50e75dbaf66efc625dcc629a564d558e
Shasmitha R <shasmitharam@gmail.com>
2025-04-12 13:19:24 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -71,7 +71,7 @@ def respond():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    '''
+'''    '''
 import random
 import json
 import pickle
@@ -130,5 +130,70 @@ def get_response_from_intents(intents_list, intents_json):
         if i['tag'] == tag:
             return random.choice(i['responses'])
     return "I'm not sure I understand."
+    ***
+    '''
+
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+# Initialize NLP tools
+lemmatizer = WordNetLemmatizer()
+
+# Load resources
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return [{'intent': classes[r[0]], 'probability': str(r[1])} for r in results]
+
+def get_response(message):
+    # Get the predicted intent from the input message
+    intents_list = predict_class(message)
+    print("Predicted intents:", intents_list)  # For debugging
+    
+    # Return the response from the intents
+    if intents_list:
+        tag = intents_list[0]['intent']
+        for i in intents['intents']:
+            if i['tag'] == tag:
+                response = random.choice(i['responses'])
+                print("Bot response:", response)  # For debugging
+                return response
+    return "Sorry, I didn't understand that."
+
+# Rename the function to avoid name clashes with the new get_response function
+def get_response_from_intents(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    for i in intents_json['intents']:
+        if i['tag'] == tag:
+            return random.choice(i['responses'])
+    return "I'm not sure I understand."
+
 
 
