32937a00e7cbad18660eb684667b0ac30d7cf301
Shasmitha R <shasmitharam@gmail.com>
2025-04-12 12:11:25 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,26 +3,28 @@ import json
 import pickle
 import numpy as np
 import nltk
-
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 lemmatizer = WordNetLemmatizer()
-#intents = json.loads(open('C:/Users/shasm/Downloads/chatbot/intents.json').read())
+
+# Load intents, words, classes, and model
 intents = json.loads(open('intents.json').read())
-
-
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.h5')
 
-
+# Function to clean up the sentence and tokenize it
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
-def bag_of_words (sentence):
+# Function to create a bag of words for the input sentence
+def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
     for w in sentence_words:
@@ -31,8 +33,9 @@ def bag_of_words (sentence):
                 bag[i] = 1
     return np.array(bag)
 
-def predict_class (sentence):
-    bow = bag_of_words (sentence)
+# Function to predict the class (intent) of the sentence
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
@@ -40,21 +43,31 @@ def predict_class (sentence):
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
-        return_list.append({'intent': classes [r[0]], 'probability': str(r[1])})
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
 
+# Function to get the response based on the intent
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if i['tag'] == tag:
-            result = random.choice (i['responses'])
+            result = random.choice(i['responses'])
             break
     return result
 
-print("GO! Bot is running!")
+# Flask route to get a response from the chatbot
+@app.route('/get_response', methods=['POST'])
+def respond():
+    data = request.get_json()  # Get the JSON data from the POST request
+    message = data.get('message')  # Extract the message
 
-while True:
-    ints = predict_class (message)
-    res = get_response (ints, intents)
-    print (res)
+    if message:
+        ints = predict_class(message)
+        response = get_response(ints, intents)
+        return jsonify({'response': response})
+    else:
+        return jsonify({'error': 'No message provided'}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
