6c0952c1076d4d2c52e1edffe84dc705490674db
Shasmitha R <shasmitharam@gmail.com>
2025-04-12 12:48:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 '''
+import os
 from flask import Flask, render_template, request, jsonify
 from chatbot import get_response  # assuming chatbot.py has get_response()
 
